2da9862c0a130e53f4dbe55e86c9cc32b74e7023
Magno <magnocruz@proton.me>
2023-02-01 22:26:00 -0300

    Adicionei a interface
    Essa é a descrição? Não sabia que dava para fazer isso. Enfim, interface bem crua, só área de texto e botão para deletar o mac. Ainda preciso gerenciar os erros.

--- main.go
+++ main.go
@@ -6,18 +6,74 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"os"
+
+	//"os"
 	"strings"
+
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/container"
+	"fyne.io/fyne/widget"
 )
 
 var device map[string]interface{}
 
 func main() {
-	quantExcluidos := 0
-	for _, id := range mac() {
-		fmt.Printf("Mac %v\n", id)
+	a := app.New()
+	w := a.NewWindow("Exportador de Macs")
+	macsInput := widget.NewMultiLineEntry()
+	content := container.NewVBox(macsInput, widget.NewButton("Deletar", func() {
+		macs := strings.Split(macsInput.Text, "\n")
+		for i := 0; i < len(macs); i++ {
+			excluir(macs[i])
+		}
+	}))
+	w.SetContent(widget.NewLabel("Excluir macs do Anlix"))
+	w.SetContent(content)
+	w.ShowAndRun()
 
-		req, err := http.NewRequest("GET", "https://app.ccomtelecom.com.br/api/v2/device/update/"+id+"/", nil)
+}
+
+func checkNilError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func checkStatus() bool {
+	if device["online_status"] == true {
+		return true
+	} else {
+		return false
+	}
+}
+
+func excluir(macs string) {
+	req, err := http.NewRequest("GET", "https://app.ccomtelecom.com.br/api/v2/device/update/"+macs+"/", nil)
+	checkNilError(err)
+	req.SetBasicAuth("magno", "10203040")
+	req.Header.Add("Content-Type", "application/json")
+	req.Close = true
+
+	client := http.Client{}
+	response, err := client.Do(req)
+	checkNilError(err)
+
+	if response.StatusCode != http.StatusOK {
+		panic("Non 2xx response from server, request" + response.Status + "\n")
+	}
+
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+	checkNilError(err)
+
+	if err := json.Unmarshal([]byte(body), &device); err != nil {
+		log.Printf("Could not unmarshal json response byte -%v\n", err)
+	}
+	if checkStatus() {
+		fmt.Printf("%v está online. Não foi excluído.\n", macs)
+
+	} else {
+		req, err := http.NewRequest("DELETE", "https://app.ccomtelecom.com.br/api/v2/device/delete/"+macs+"/", nil)
 		checkNilError(err)
 		req.SetBasicAuth("magno", "10203040")
 		req.Header.Add("Content-Type", "application/json")
@@ -30,58 +86,5 @@ func main() {
 		if response.StatusCode != http.StatusOK {
 			panic("Non 2xx response from server, request" + response.Status + "\n")
 		}
-
-		defer response.Body.Close()
-		body, err := io.ReadAll(response.Body)
-		checkNilError(err)
-
-		if err := json.Unmarshal([]byte(body), &device); err != nil {
-			log.Printf("Could not unmarshal json response byte -%v\n", err)
-		}
-		if checkStatus() {
-			fmt.Printf("%v está online. Não foi excluído.\n", id)
-
-		} else {
-			req, err := http.NewRequest("DELETE", "https://app.ccomtelecom.com.br/api/v2/device/delete/"+id+"/", nil)
-			checkNilError(err)
-			req.SetBasicAuth("magno", "10203040")
-			req.Header.Add("Content-Type", "application/json")
-			req.Close = true
-
-			client := http.Client{}
-			response, err := client.Do(req)
-			checkNilError(err)
-
-			if response.StatusCode != http.StatusOK {
-				panic("Non 2xx response from server, request" + response.Status + "\n")
-			}
-			fmt.Printf("%v foi excluído.\n", id)
-		}
-		quantExcluidos++
-	}
-	fmt.Printf("Foram excluídos %v aparelhos.\n", quantExcluidos)
-
-}
-
-func checkNilError(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-func mac() []string {
-	macsList, err := os.ReadFile("excluir do anlix.txt")
-	checkNilError(err)
-	macs := string(macsList[:])
-
-	mac := strings.Fields(macs)
-	return mac
-}
-
-func checkStatus() bool {
-	if device["online_status"] == true {
-		return true
-	} else {
-		return false
 	}
 }
